0d351e507633f294eb23f31638f3d69fd0e8701f
camiloruiz <camiloruizvidal@gmail.com>
2022-10-23 14:03:35 -0500

    delete function

--- back/src/app/Routes/items.route.ts
+++ back/src/app/Routes/items.route.ts
@@ -5,7 +5,6 @@ import ItemsController from "../Controllers/Items.controller";
 const router = Router();
 
 router.get('/:id', ItemsController.getItem)
-router.get('/:id/description', ItemsController.getDescription)
-router.get('/', ItemsController.getQuery)
+router.get('', ItemsController.getQuery)
 
 export default router;
